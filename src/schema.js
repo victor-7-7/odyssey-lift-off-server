ba87987571f4052284955790809864cbe863b732
victor-7-7 <Victor.7.7@ya.ru>
2023-02-27 13:19:29 +0700

    Remove 'length' schema field

--- src/schema.js
+++ src/schema.js
@@ -69,8 +69,8 @@ const typeDefs = gql`
         author: Author!
         "The track's main illustration to display in track card or track page detail"
         thumbnail: String
-        "The track's approximate length to complete, in seconds"
-        length: Int @deprecated(reason: "Use durationInSeconds")
+#        "The track's approximate length to complete, in seconds"
+#        length: Int @deprecated(reason: "Use durationInSeconds")
         "The track's full duration, in seconds"
         durationInSeconds: Int
         "The number of modules this track contains"
@@ -99,8 +99,8 @@ const typeDefs = gql`
         id: ID!
         "The Module's title"
         title: String!
-        "The Module's length in seconds"
-        length: Int @deprecated(reason: "Use durationInSeconds")
+#        "The Module's length in seconds"
+#        length: Int @deprecated(reason: "Use durationInSeconds")
         "The module's video duration, in seconds"
         durationInSeconds: Int
         "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
